e8c86163fb6a2695074548d1f59c35ec55f3e6dc
Sunjay Varma <varma.sunjay@gmail.com>
2018-01-20 01:10:20 -0500

    Added command line argument parsing

--- src/bin/logoc.rs
+++ src/bin/logoc.rs
@@ -1,24 +1,49 @@
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
 extern crate logoc;
 
 use std::io::Write;
 use std::fs::{self, File};
 use std::process::{self, Command};
-use std::path::Path;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
 
 use logoc::ast::{Program, Instruction, Expr};
 use logoc::codegen;
 use logoc::cargo;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "logoc")]
+struct CompilerOptions {
+    /// The directory to place all build artifacts in
+    #[structopt(long = "build-dir", default_value = "build", parse(from_os_str),
+        help = "The directory to place build artifacts and the generated executable")]
+    build_dir: PathBuf,
+    /// The path of the LOGO program to compile
+    #[structopt(parse(from_os_str), help = "The path of the LOGO program to compile")]
+    program: PathBuf,
+    /// The filename (without extension) of the output executable
+    #[structopt(short = "o", long = "output-file",
+        help = "The filename (without extension) of the output executable")]
+    output: Option<String>,
+    /// Whether to format the generated code
+    #[structopt(long = "fmt", help = "Whether to format the generated Rust code")]
+    format: bool,
+}
+
 fn main() {
-    //TODO: Get these from command line args
-    // The directory to place all build artifacts in
-    let build_dir = Path::new("build");
-    // The filename (without extension) of the output executable
-    let output = "square";
-    // The path of the intermediate Rust file that will be generated
-    let output_path = format!("{}.rs", output);
-    // Whether to format the generated code
-    let rustfmt = true;
+    let CompilerOptions {
+        build_dir,
+        program,
+        output,
+        format,
+    } = CompilerOptions::from_args();
+    let output = &output.unwrap_or_else(|| program.file_stem()
+        .expect("Please provide a valid filename to a LOGO program").to_str()
+        .expect("LOGO program filename was not valid unicode").to_owned());
 
     //TODO: Read from input file that is provided by command line args
     let ast: Program = vec![
@@ -31,23 +56,26 @@ fn main() {
         Instruction::Forward(Expr::Number(100.0)),
         Instruction::Right(Expr::Number(90.0)),
     ];
-    let code = codegen::to_tokens(ast).unwrap();
+    let rust_code = codegen::to_tokens(ast).unwrap();
 
-    fs::create_dir_all(build_dir)
+    fs::create_dir_all(&build_dir)
         .expect("Failed to create directory for build output");
 
-    let output_code = build_dir.join(&output_path);
-    let mut output_code = File::create(output_code).unwrap();
-    writeln!(output_code, "{}", code.as_str()).unwrap();
+
+    // The path of the intermediate Rust file that will be generated
+    let rust_filename = &format!("{}.rs", output);
+    let rust_file_path = &build_dir.join(rust_filename);
+    let mut rust_code_file = File::create(rust_file_path).unwrap();
+    writeln!(rust_code_file, "{}", rust_code.as_str()).unwrap();
 
     let output_cargo_cfg = build_dir.join("Cargo.toml");
     let output_cargo_cfg = File::create(output_cargo_cfg).unwrap();
-    cargo::write_cargo_toml(output_cargo_cfg, output, &output_path);
+    cargo::write_cargo_toml(output_cargo_cfg, output, rust_filename);
 
-    if rustfmt {
+    if format {
         let status = Command::new("rustfmt")
-            .arg(output_path)
-            .current_dir(build_dir)
+            .arg(rust_filename)
+            .current_dir(&build_dir)
             .status()
             .expect("Failed to execute rustfmt -- make sure you have rustfmt installed");
         if !status.success() {
@@ -61,7 +89,7 @@ fn main() {
     // Run the generated Rust code
     let status = Command::new("cargo")
         .arg("build")
-        .current_dir(build_dir)
+        .current_dir(&build_dir)
         .status()
         .expect("Failed to execute Rust compiler -- make sure you have Rust and Cargo installed.");
 
